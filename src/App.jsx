e738380ce01a59a429193aff4e07e660093cae32
unknown <danimanalu755-spec>
2026-09-05 22:59:41 +0700

    mengganti foto profil asli dan terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function App() {
           <p className="section-intro">Silakan hubungi saya untuk kolaborasi atau sekadar menyapa!</p>
           <div style={{ display: 'flex', flexDirection: 'column', gap: '15px', alignItems: 'center', marginTop: '30px' }}>
             <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}><FaEnvelope color="#61DAFB" size="20px" /><span>danimanalu755@gmail.com</span></div>
-            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}><FaPhone color="#61DAFB" size="20px" /><span>+62 852-6027-7250</span></div>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}><FaPhone color="#61DAFB" size="20px" /><span>+62 85`12345667`</span></div>
             <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}><FaMapMarkerAlt color="#E34F26" size="20px" /><span>Sumatera Utara, Indonesia</span></div>
           </div>
         </Reveal>
